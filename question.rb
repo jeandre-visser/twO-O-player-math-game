3577a2533afe1e5aae64c43ed582f793b01e42bc
Jeandre Visser <jeandrev1414@gmail.com>
2022-08-22 19:27:09 +0000

    Add getter

--- question.rb
+++ question.rb
@@ -1,10 +1,16 @@
 class Question
   def initializer
-    @num1 = rand(0...21)
-    @num2 = rand(0...21)
+    @num1 = rand(21)
+    @num2 = rand(21)
   end
+
+  attr_reader :num1, :num2
    
   def nxt_question
     "What does #{num1} plus #{num2} equal?"
   end
 end
+
+qs1 = Question.new
+puts qs1.nxt_question
+puts qs1.num1
