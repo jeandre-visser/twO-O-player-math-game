6358473ed49e929a56b3d59be0f0eafd315272d1
Jeandre Visser <jeandrev1414@gmail.com>
2022-08-22 23:15:22 +0000

    Add correct method and change random number to not include 0

--- question.rb
+++ question.rb
@@ -1,7 +1,8 @@
 class Question
   def initialize
-    @num1 = rand(21)
-    @num2 = rand(21)
+    # Generate two random numbers
+    @num1 = rand(1...21)
+    @num2 = rand(1...21)
   end
 
   attr_reader :num1, :num2
@@ -9,7 +10,10 @@ class Question
   def nxt_question
     "What does #{num1} plus #{num2} equal?"
   end
+
+  def correct
+    num1 + num2
+  end
 end
 
-qs1 = Question.new
-puts qs1.nxt_question
+
