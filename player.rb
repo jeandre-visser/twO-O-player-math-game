e7dcc662877dee9eb89d0fead5e692a6537ba3c0
Jeandre Visser <jeandrev1414@gmail.com>
2022-08-22 23:14:39 +0000

    Remove unnecassary states and add id

--- player.rb
+++ player.rb
@@ -1,24 +1,14 @@
-require "pry"
-
 class Player
-  @@num_of_player = 0
 
-  def initialize
-    @@num_of_player += 1
-    @player_long = "Player #{@@num_of_player}"
-    @player_short = "P#{@@num_of_player}"
+  def initialize(id)
+    @id = id
     @player_score = 3
   end
   
-  attr_reader :player_long, :player_short, :player_score
+  attr_reader :id, :player_score
 
   def losing_score
     @player_score -= 1
   end
-
-  def final_score
-    "#{player_score}/3"
-  end
-
-  attr_writer :player_long, :player_short, :player_score
+  
 end
