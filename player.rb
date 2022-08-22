0cde24f0f1cea527d01cd03c89afb2f00eeef6b3
Jeandre Visser <jeandrev1414@gmail.com>
2022-08-22 18:44:45 +0000

    Add states to initializer and get them using attr_reader

--- player.rb
+++ player.rb
@@ -1,10 +1,14 @@
 class Player
-  @@num_of_players = 0
+  @@num_of_player = 0
 
   def initializer
-    @@num_of_players += 1
-    
+    @@num_of_player += 1
+    @player_long = "Player #{@@num_of_player}"
+    @player_short = "P#{@@num_of_player}"
+    @player_score = 3
   end
+  
+  attr_reader :player_long, :player_short, :player_score
 
   
 end
